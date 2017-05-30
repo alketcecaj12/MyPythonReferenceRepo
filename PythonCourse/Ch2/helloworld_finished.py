be12da6a7ce58eae44c286c79c84729dc8980099
tekrar <alket.cecaj@.gmail.com>
2017-05-30 08:35:50 +0200

    python code

---
+++ PythonCourse/Ch2/helloworld_finished.py
@@ -0,0 +1,13 @@
+#
+# Example file for HelloWorld
+# (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
+
+def main():
+  print "hello world!"
+  print "Ciao People!"
+  
+
+print "ciao again"
+    
+if __name__ == "__main__":
+  main()
